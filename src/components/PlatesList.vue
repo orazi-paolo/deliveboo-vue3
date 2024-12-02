8550e2f77ba07c2f634eca5fa1d861ab262a174a
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-02 17:36:49 +0100

    created component PlatesList

---
+++ src/components/PlatesList.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name:"Plateslist",  
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+  
+</style>

5bf22cb8cc8cd7e126b6f8a37e9ea51bfaae3038
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-03 12:06:51 +0100

    created AppSingleRestaurant in pages and imported PlatesList component

---
+++ src/pages/AppSingleRestaurant.vue
@@ -0,0 +1,29 @@
+<script>
+import PlatesList from '../components/plates/PlatesList.vue';
+export default {
+  name:"AppSingleRestaurant",  
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    PlatesList,
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+    <section id="AppSingleRestaurant">
+        <div>
+            <PlatesList />
+        </div>
+    </section>
+</template>
+
+<style lang="scss">
+  
+</style>

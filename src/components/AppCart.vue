9af9eaf7115a66878d83f70fabbfa184f3635b78
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-03 12:44:59 +0100

    created AppCart component

---
+++ src/components/AppCart.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name:"AppCart",  
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+  
+</style>

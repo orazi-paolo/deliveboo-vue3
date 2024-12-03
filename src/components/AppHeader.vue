b6a32c8c40b276ebf6c75e66e4f1835d7f909e24
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-03 12:27:19 +0100

    created AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+    <header>
+        <nav class="navbar navbar-expand bg-success">
+            <div class="container-fluid">
+                <a class="navbar-brand fw-bolder" href="#">Deliveboo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link class="nav-link active" aria-current="page" :to="{ 'name' : 'restaurants.show'}">Restaurant</router-link>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>

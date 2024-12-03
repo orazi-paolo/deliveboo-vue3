c221f7fa615fa0ab7c6f3fa92a26d922b4e1cf93
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-03 11:35:26 +0100

    added style for li #plate-card in PlatesListCard

--- src/components/plates/PlatesListCard.vue
+++ src/components/plates/PlatesListCard.vue
@@ -22,11 +22,74 @@ export default {
 </script>
 
 <template>
-  <li>
-    {{ plateObj.name }}
+  <!-- card of single plate -->
+  <li id="plate-card">
+    <div class="info-plate-card">
+      <h4>{{ plateObj.name }}</h4>
+      <p>{{ plateObj.description }}</p>
+      <div class="price">{{ plateObj.price }} <span>&euro;</span> </div>
+    </div>
+    <div class="box-img">
+      <img :src="plateObj.image" :alt="`Image of ${plateObj.name}`">    
+    </div>
+    <!-- button add to cart -->
+    <button class="add-item"><span>+</span></button>
   </li>
 </template>
 
 <style lang="scss" scoped>
+  #plate-card{
+    flex-basis: calc(50% - 15px);
+    display: flex;
+    gap: 10px;
+    border: 1px solid rgb(230, 217, 217);
+    padding: 15px 12px;
+    min-width: 375px;
+
+    &:hover {
+      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+    }
+
+    .info-plate-card{
+      flex-grow: 1;
+
+      h4{
+        font-size: 17px;
+      }
+
+      p, h4{
+       margin-bottom: 8px; 
+      }
+
+      p, .price{
+        font-size: 14px;
+      }
+    } 
+
+    .box-img{
+      flex-basis: 100px;
+
+      img{
+        display: block;
+        max-width: 100%;
+        max-height: 100%;
+        object-fit: cover;
+      }
+    }
+
+    .box-img, .add-item{
+      max-height: 90px;
+      border: 1px solid rgb(230, 217, 217);
+    }
+
+    .add-item{
+      flex-basis: 40px;
+      background-color: #fff;
+      span{
+        color: #70C8BF;
+        font-size: 20px;
+      }
+    }
+  }
   
 </style>

5468c30b13dc2733525b28219dd2ac60e7eb0202
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-04 10:34:46 +0100

    created AppListRestaurants page and imported RestaurantsList

---
+++ src/pages/AppListRestaurants.vue
@@ -0,0 +1,29 @@
+<script>
+import RestaurantsList from '../components/restaurants/RestaurantsList.vue';
+export default {
+  name:"AppListRestaurants",
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    RestaurantsList
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+    <div class="mx-5 mt-4">  <!-- simulazione container-lg -->
+        <section id="AppListRestaurants">
+            <RestaurantsList/>            
+        </section>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>

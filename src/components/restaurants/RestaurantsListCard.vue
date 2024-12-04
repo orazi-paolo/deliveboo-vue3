12cce0d8ccd8c377240655da7d763df32dc95a60
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-04 10:31:23 +0100

    created RestaurantsListCard component

---
+++ src/components/restaurants/RestaurantsListCard.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name:"RestaurantsListCard",  
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+  
+</style>

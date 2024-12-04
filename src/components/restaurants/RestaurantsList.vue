b8072e8cec2f4e802c6f562caddbafe97463ff3d
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-04 10:32:06 +0100

    created RestaurantsList component and imported RestaurantsListCard

---
+++ src/components/restaurants/RestaurantsList.vue
@@ -0,0 +1,25 @@
+<script>
+import RestaurantsListCard from './RestaurantsListCard.vue';
+export default {
+  name:"RestaurantsList",  
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    RestaurantsListCard
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+  <RestaurantsListCard />
+</template>
+
+<style lang="scss" scoped>
+  
+</style>

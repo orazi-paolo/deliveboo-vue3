38f7229460560fe820f040d57956ce0b2c1a5a65
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-07 11:33:33 +0100

    created TipologiesList component and imported TipologieListCard component

---
+++ src/components/tipologies/TipologiesList.vue
@@ -0,0 +1,32 @@
+<script>
+import TipologiesListCard from './TipologiesListCard.vue';
+export default {
+  name: "TipologiesList",  
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    TipologiesListCard,
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+  <ul id="tipologies-list" class="row">
+    <TipologiesListCard />
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+  #tipologies-list {
+    flex-wrap: wrap;
+    padding: 0;
+    list-style-type: none;
+    display: flex;
+}
+</style>

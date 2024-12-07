dafc208130dbe2fddff42a8fc9cea5d1eeb2c7c7
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-07 11:12:33 +0100

    created Home.vue page

---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name:"Home",  
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+  
+</style>

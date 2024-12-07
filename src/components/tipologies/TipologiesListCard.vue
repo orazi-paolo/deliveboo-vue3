5f5420e8742e1a78ca4125b349ca05054639d2d2
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-07 11:32:31 +0100

    created TipologiesListCard.vue component

---
+++ src/components/tipologies/TipologiesListCard.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "TipologiesListCard",
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+  <!-- card of single tipology -->
+  <li class="col-3 p-1 p-lg-2">
+    <div id="tipology-card">
+    </div>
+  </li>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>

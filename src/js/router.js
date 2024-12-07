25957ae578e21eeff0e5769e45a5470e4695e7f6
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-07 11:13:14 +0100

    added route for Home in router.js

--- src/js/router.js
+++ src/js/router.js
@@ -2,11 +2,12 @@ import { createWebHistory, createRouter } from 'vue-router'
 import AppListRestaurants from '../pages/AppListRestaurants.vue'
 import AppSingleRestaurant from '../pages/AppSingleRestaurant.vue'
 // import PlateShow from '../components/PlateShow.vue'
+import Home from '../pages/Home.vue'
 
 const routes = [
-  { path: "/", component: AppListRestaurants, name: "restaurants" },
+  { path: "/", component: Home, name: "home" },
+  { path: "/restaurants", component: AppListRestaurants, name: "restaurants" },
   { path: "/restaurants/:id", component: AppSingleRestaurant, name: "restaurants.show" },
-  // { path: "/restaurants/test", component: AppSingleRestaurant, name: "restaurants.show" },
   /* { path: '/plate/:id', name: 'plates.show', component: PlateShow }, */
 ]
 

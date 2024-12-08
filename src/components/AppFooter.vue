0ee161911183b767a716d8998522d6b8207c7857
natdm02 <natalidimarino2002@gmail.com>
2024-12-08 22:13:04 +0100

    [add] renewed css structure

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -11,15 +11,6 @@ export default {
   <footer class="bg-dark text-light py-5">
 
     <div class="container-custom">
-      <!-- take DeliveBoo section-->
-      <div class="take-deliveboo mb-5 text-center p-3">
-        <h5 class="text-uppercase">Take DeliveBoo with you</h5>
-        <div class="d-flex justify-content-center gap-3">
-          <img src="/public/app-store-logo.png" alt="App Store" class="store-logo">
-          <img src="/public/Playstore.png" alt="Google Play" class="store-logo">
-        </div>
-      </div>
-
       <!-- main columns -->
       <div class="row">
         <!-- Column 1 -->
@@ -95,16 +86,11 @@ export default {
 </template>
 
 <style lang="scss">
+
 footer {
   background-color: #2c2c2c;
   color: #f1f1f1;
   padding: 3rem 0;
-
-  // .container {
-  //   display: flex;
-  //   flex-direction: column;
-  //   gap: 2rem;
-  // }ù
 
   .border-top {
     border-top: 1px solid #444;
@@ -124,7 +110,6 @@ footer {
         color: #1877f2;
       }
     }
-
 
     .fa-facebook:hover {
       color: #1877f2;
@@ -151,26 +136,12 @@ footer {
   }
 
   .take-deliveboo {
-    // text-align: center;
-    // margin-bottom: 2rem;
-    // border-radius: 0.5rem;
-    // background-color: #333333;
-
     h5 {
       font-weight: 700;
       font-size: 1.2rem;
       color: #ffffff;
       margin-bottom: 1rem;
-      // text-align: center;
-      // text-transform: uppercase;
-      // letter-spacing: 1px; 
-      // color: #ffffff; 
     }
-
-
-    // ul li  a{
-    // margin-bottom: 1rem; 
-    // }
 
     .store-logo {
       width: 120px;
@@ -182,15 +153,9 @@ footer {
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
       }
     }
-
-
   }
 
-
-
-
   .row {
-    // display: flex;
     justify-content: center;
     gap: 1rem;
 
@@ -198,10 +163,7 @@ footer {
       background-color: #333333;
       padding: 1.5rem;
       border-radius: 0.5rem;
-      // text-align: left;
-      // width: 100%;
-      // flex: 1;
-      // min-width: 200px;
+      text-align: left;
 
       h5 {
         font-size: 1rem;
@@ -218,22 +180,19 @@ footer {
         li {
           margin-bottom: 0.5rem;
 
-          .footer-link {
+            a {
             color: #ddd;
             text-decoration: none;
             transition: color 0.3s ease;
-            font-size: 0.9rem;
 
-            &:hover {
+              &:hover {
               color: #00c7b6;
+              }
             }
-          }
         }
       }
     }
   }
-
-
 
   img {
     display: inline-block;
@@ -247,15 +206,11 @@ footer {
     }
   }
 
-  .border-top {
-    border-top: 1px solid #555555;
-    margin-top: 2rem;
-  }
-
   p {
     font-size: 0.9rem;
     color: #bbbbbb;
     margin: 0;
   }
 }
+
 </style>

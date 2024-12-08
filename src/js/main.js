6dfcce66ae29e33c11a78c01903a10a305a9b76a
Marcap00 <cappellatomarco@yahoo.com>
2024-12-08 21:18:12 +0100

    fixed merge

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,10 @@
-import { createApp } from 'vue'
-import router from './router'
-import App from '../App.vue'
 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
-
-createApp(App).use(router).mount('#app')
 import { createApp } from 'vue';
 import App from '../App.vue';
 import router from './router';
 
 // Importazione FontAwesome
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';

7460a8c198b966a5b319d594e5717d421ca5c185
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-11 12:36:55 +0100

    created page AppCheckout and imported OrderReview

---
+++ src/pages/AppCheckout.vue
@@ -0,0 +1,27 @@
+<script>
+import OrderReview from "../components/OrderReview.vue"
+export default {
+    name: "AppCheckout",
+    data() {
+        return {
+
+        };
+    },
+    components: {
+        OrderReview
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<template>
+    <section id="AppCheckout">
+        <div class="container-custom my-4"> <!-- to costumize container-custom go to style/general.scss -->
+            <OrderReview />
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped></style>

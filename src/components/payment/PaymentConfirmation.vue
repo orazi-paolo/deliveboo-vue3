08398f360565306737572a853400a9608a08938e
natdm02 <natalidimarino2002@gmail.com>
2024-12-12 21:56:52 +0100

    [add] basic layout

--- src/components/payment/PaymentConfirmation.vue
+++ src/components/payment/PaymentConfirmation.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      
+        order: null,
     };
   },
   methods: {
@@ -10,15 +10,25 @@ export default {
   },
   mounted() {
 
+    // //dettagli ordine
+    // this.order = JSON.parse(localStorage.getItem("orderDetails"));
+
   },
 };
 </script>
 
 <template>
 
-    <h1>ciao</h1>
+    <div class="container py-5">
+        <div class="text-center">
+            <h1 class="text-success">Complimenti!</h1>
+                <p class="fs-5">Il pagamento è stato completato con successo.</p>
+                <p class="fs-6">Abbiamo inviato una email di conferma al tuo indirizzo.</p>
+        </div>
+    </div>
 
 </template>
+
 
 
 <script scoped>

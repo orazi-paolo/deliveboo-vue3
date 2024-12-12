07d786a77d2b441811b183139669b31290e9962e
natdm02 <natalidimarino2002@gmail.com>
2024-12-12 21:25:38 +0100

    [add] scaffolding

---
+++ src/components/payment/PaymentConfirmation.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  },
+  mounted() {
+
+  },
+};
+</script>
+
+<template>
+
+    <h1>ciao</h1>
+
+</template>
+
+
+<script scoped>
+
+</script>

3dfa166e08e13e7ac86477b4aa7f7eddb22dd58c
Marcap00 <cappellatomarco@yahoo.com>
2024-12-14 15:41:48 +0100

    add text decoration none link header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -20,25 +20,12 @@ export default {
         <div class="nav-left">
           <ul class="pages-links">
             <li class="li-logo">
-              <img
-                class="img-logo"
-                src="./../assets/deliveroo-logo.webp"
-                alt="deliveroo-logo.web"
-              />
-              <router-link
-                class="nav-link active fw-bold"
-                aria-current="page"
-                :to="{ name: 'home' }"
-                >deliveboo</router-link
-              >
+              <img class="img-logo" src="./../assets/deliveroo-logo.webp" alt="deliveroo-logo.web" />
+              <router-link class="nav-link active fw-bold" aria-current="page"
+                :to="{ name: 'home' }">deliveboo</router-link>
             </li>
             <li class="nav-item">
-              <router-link
-                class="nav-link active"
-                aria-current="page"
-                :to="{ name: 'home' }"
-                >Home</router-link
-              >
+              <router-link class="nav-link active" aria-current="page" :to="{ name: 'home' }">Home</router-link>
             </li>
           </ul>
         </div>
@@ -46,13 +33,10 @@ export default {
           <!-- <router-link :to="{ name: 'resolveTransitionHooks.show' }">
             </router-link> -->
           <ul class="auth flex-column flex-sm-row">
-            <router-link
-              :to="{
-                name: 'restaurants.show',
-                params: { slug: store.platesInCart[0].restaurant.slug },
-              }"
-              v-if="store.platesInCart.length > 0 && $route.path === '/'"
-            >
+            <router-link :to="{
+              name: 'restaurants.show',
+              params: { slug: store.platesInCart[0].restaurant.slug },
+            }" v-if="store.platesInCart.length > 0 && $route.path === '/'">
               <li class="cart-button">
                 <font-awesome-icon icon="cart-shopping" />
                 <small class="cart-quantities">{{
@@ -61,23 +45,13 @@ export default {
               </li>
             </router-link>
             <li class="register-button">
-              <font-awesome-icon
-                :icon="['fas', 'house-chimney']"
-                class="icon-auth"
-              />
-              <a
-                href="http://127.0.0.1:8000/login"
-                class="menu-item fw-bold turquoise"
-                >Login</a
-              >
+              <font-awesome-icon :icon="['fas', 'house-chimney']" class="icon-auth" />
+              <a href="http://127.0.0.1:8000/login" class="menu-item fw-bold turquoise text-decoration-none">Login</a>
             </li>
             <li class="sign-in-button">
               <font-awesome-icon :icon="['fas', 'user']" class="icon-auth" />
-              <a
-                href="http://127.0.0.1:8000/register"
-                class="menu-item fw-bold turquoise"
-                >Register</a
-              >
+              <a href="http://127.0.0.1:8000/register"
+                class="menu-item fw-bold turquoise text-decoration-none">Register</a>
             </li>
           </ul>
         </div>
@@ -129,8 +103,8 @@ nav {
       border-radius: 5px;
       padding: 5px 25px;
 
-      .cart-quantities{
-        color:#00cbbd;
+      .cart-quantities {
+        color: #00cbbd;
       }
 
       .icon-auth {

46f7e12b31bc2801208ada179277bdae56d6474e
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-12-17 12:45:54 +0100

    fixed Appheader's nav-toogle

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,42 +6,42 @@ export default {
   data() {
     return {
       store,
+      isNavbarOpen: false,
     };
   },
-  components: {},
-  methods: {},
+  methods: {
+    toggleNavbar() {
+      this.isNavbarOpen = !this.isNavbarOpen;
+    },
+  },
 };
 </script>
 
 <template>
   <header class="sticky-top shadow">
-    <nav class="navbar navbar-expand">
-      <div class="container-fluid">
-        <div class="nav-left">
-          <ul class="pages-links">
-            <li class="li-logo">
-              <img class="img-logo" src="./../assets/deliveroo-logo.webp" alt="deliveroo-logo.web" />
-              <router-link class="title-logo nav-link active fw-bold" aria-current="page"
-                :to="{ name: 'home' }">deliveboo</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" :to="{ name: 'home' }">Home</router-link>
-            </li>
-          </ul>
-        </div>
-        <div class="nav-right">
-          <!-- <router-link :to="{ name: 'resolveTransitionHooks.show' }">
-            </router-link> -->
-          <ul class="auth flex-column flex-sm-row">
-            <router-link :to="{
-              name: 'restaurants.show',
-              params: { slug: store.platesInCart[0].restaurant.slug },
-            }" v-if="store.platesInCart.length > 0 && $route.path === '/'">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm px-3 px-xl-5">
+      <div class="nav-left d-flex align-items-center justify-content-between w-100">
+        <ul class="pages-links d-flex align-items-center">
+          <li class="li-logo">
+            <img class="img-logo" src="./../assets/deliveroo-logo.webp" alt="deliveroo-logo.webp" />
+            <router-link class="nav-link active fw-bold" aria-current="page" :to="{ name: 'home' }">deliveboo</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" :to="{ name: 'home' }">Home</router-link>
+          </li>
+        </ul>
+
+        <button class="navbar-toggler" type="button" @click="toggleNavbar" :aria-expanded="isNavbarOpen" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" :class="{ 'show': isNavbarOpen }" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto auth flex-column flex-md-row">
+            <router-link v-if="store.platesInCart.length > 0 && $route.path === '/'"
+              :to="{ name: 'restaurants.show', params: { slug: store.platesInCart[0].restaurant.slug } }">
               <li class="cart-button">
                 <font-awesome-icon icon="cart-shopping" />
-                <small class="cart-quantities">{{
-                  store.totalQuantities
-                }}</small>
+                <small class="cart-quantities">{{ store.totalQuantities }}</small>
               </li>
             </router-link>
             <li class="register-button">
@@ -50,8 +50,7 @@ export default {
             </li>
             <li class="sign-in-button">
               <font-awesome-icon :icon="['fas', 'user']" class="icon-auth" />
-              <a href="http://127.0.0.1:8000/register"
-                class="menu-item fw-bold turquoise text-decoration-none">Register</a>
+              <a href="http://127.0.0.1:8000/register" class="menu-item fw-bold turquoise text-decoration-none">Register</a>
             </li>
           </ul>
         </div>
@@ -60,12 +59,11 @@ export default {
   </header>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 nav {
   background-color: #fff;
   padding: 15px 25px;
   border-bottom: 1px solid rgb(230, 217, 217);
-  min-height: 70px;
 
   * {
     color: #00cbbd;
@@ -75,30 +73,17 @@ nav {
   .nav-left {
     display: flex;
     align-items: center;
+    justify-content: space-between;
 
     .li-logo {
       display: flex;
       align-items: center;
       gap: 5px;
 
-      .title-logo {
-        font-size: 18px;
-        font-weight: 700;
-      }
-
       .img-logo {
         width: 25px;
       }
     }
-
-    .nav-item {
-      height: 23px;
-    }
-  }
-
-  .pages-links {
-    display: flex;
-    align-items: center;
   }
 
   .pages-links,
@@ -128,5 +113,34 @@ nav {
       }
     }
   }
+
+  .navbar-toggler {
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
 }
+
+@media (max-width: 768px) {
+    .collapse {
+      display: none;
+    }
+
+    .collapse.show {
+      display: flex;
+      flex-direction: column; 
+      position: fixed; 
+      top: 47px; 
+      right: 10px;
+      background-color: #ddd;
+      border: 1px solid #aaa;
+      padding: 5px;
+      border-radius: 10px;
+    }
+  }
+}
+
+
+
 </style>
